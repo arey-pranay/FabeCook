d2fab72d1c0a64124d59623b06c1af9e223bca6a
arey-pranay <pranayparikh2004@gmail.com>
2023-08-24 15:38:31 +0530

    added setup notes regarrding status

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -4,8 +4,3 @@ exports.f1 = (req, res) => {
     error: "irjfefi",
   });
 };
-//200 means all good
-//status 204 means you won't get body, but the request was successful
-//404 means not found
-//400, bad request or error
-//403, means forbidden, cannot be authenticated
