aad5307f5fd1babdeda1ce7cc46a52d2b5e2955a
arey-pranay <pranayparikh2004@gmail.com>
2023-08-25 17:07:54 +0530

    adduser module

---
+++ backend/models/User.js
@@ -0,0 +1,137 @@
+const mongoose = require("mongoose");
+const userSchema = mongoose.Schema(
+  {
+    first_name: {
+      type: String,
+      required: [true, "first name is required"],
+      trim: true,
+      text: true,
+    },
+    last_name: {
+      type: String,
+      required: [true, "last name is required"],
+      trim: true,
+      text: true,
+    },
+    username: {
+      type: String,
+      required: [true, "username is required"],
+      trim: true,
+      text: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    picture: {
+      type: String,
+      default:
+        "https://static.vecteezy.com/system/resources/thumbnails/020/765/399/small/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg",
+    },
+    cover: {
+      type: String,
+    },
+    gender: {
+      type: String,
+      required: [true, "gender is required"],
+      trim: true,
+    },
+    bYear: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+    bMonth: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+    bDay: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    following: {
+      type: Array,
+      default: [],
+    },
+    requests: {
+      type: Array,
+      default: [],
+    },
+    //history
+    search: [
+      {
+        user: {
+          type: ObjectId,
+          ref: "User",
+        },
+      },
+    ],
+    details: {
+      bio: {
+        type: String,
+      },
+      otherName: {
+        type: String,
+      },
+      job: {
+        type: String,
+      },
+      workplace: {
+        type: String,
+      },
+      highSchool: {
+        type: String,
+      },
+      college: {
+        type: String,
+      },
+      currentCity: {
+        type: String,
+      },
+      relationship: {
+        type: String,
+        enum: ['"Single","In A Relationship","Married","Divorced"'],
+      },
+      instagram: {
+        type: String,
+      },
+    },
+    //putting it in user instead of a separate module to get advanced search
+    savedPost: [
+      {
+        post: {
+          type: ObjectId,
+          ref: "Post",
+        },
+        savedAt: {
+          type: Date,
+          default: new Date(),
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+module.exports = mongoose.model("User");
