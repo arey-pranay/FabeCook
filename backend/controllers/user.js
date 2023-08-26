2f5fb3798abc090fbb13a9e4667a9eea80b5939d
arey-pranay <pranayparikh2004@gmail.com>
2023-08-26 16:14:59 +0530

    added register and request-posting

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,10 @@
-exports.f1 = (req, res) => {
-  res.status(404).json({
-    messsage: "aofjeoijeo",
-    error: "irjfefi",
-  });
+// exports.f1 = (req, res) => {
+//   res.status(404).json({
+//     messsage: "aofjeoijeo",
+//     error: "irjfefi",
+//   });
+// };
+
+exports.register = (req, res) => {
+  console.log(req.body);
 };
