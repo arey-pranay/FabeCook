af6e6ec59c565245c86a5d06297d02631e17bffc
arey-pranay <pranayparikh2004@gmail.com>
2023-09-21 15:04:44 +0530

    Header Section

---
+++ frontend/src/data/allMenu.js
@@ -0,0 +1,175 @@
+export const menu = [
+  {
+    name: "Campus",
+    icon: "campus",
+    description: "A unique, exclusive space for college students on Facebook.",
+  },
+  {
+    name: "Events",
+    icon: "events",
+    description: "Organize or find events in your campus.",
+  },
+  {
+    name: "Find Friends",
+    icon: "friends",
+    description: "Search for friends or people you may know.",
+  },
+  {
+    name: "Groups",
+    icon: "groups",
+    description: "Connect with people who share your interests.",
+  },
+  {
+    name: "My Feed",
+    icon: "feed",
+    description: "See relevant posts from people and Pages you follow.",
+  },
+  {
+    name: "Pages",
+    icon: "pages",
+    description: "Find your clubs and chapters to know about them",
+  },
+  {
+    name: "Hire People",
+    icon: "marketplace",
+    description: "Need people to work with your club or startups.",
+  },
+  {
+    name: "Gaming Video",
+    icon: "gaming",
+    description: "Watch and connect with your favorite games and streamers.",
+  },
+  {
+    name: "Play Games",
+    icon: "play",
+    description: "Play your favorite games.",
+  },
+  {
+    name: "Watch",
+    icon: "watch",
+    description:
+      "A video destination personalized to your interests and connections.",
+  },
+  {
+    name: "Facebook Pay",
+    icon: "pay",
+    description: "A seamless, secure way to pay on the apps you already use.",
+  },
+  {
+    name: "Marketplace",
+    icon: "marketplace",
+    description: "Buy and sell in your community.",
+  },
+  {
+    name: "Recent Activity",
+    icon: "recent",
+    description: "See all the posts you interacted with on ViMeet.",
+  },
+  {
+    name: "Memories",
+    icon: "memories",
+    description: "Browse your old photos, videos and posts on ViMeet.",
+  },
+  {
+    name: "Saved",
+    icon: "saved",
+    description: "Find posts, photos and videos that you saved for later.",
+  },
+  {
+    name: "Weather",
+    icon: "weather",
+    description:
+      "Check your local forecast and sign up for daily weather notifications.",
+  },
+  {
+    name: "Ads",
+    icon: "ads",
+    description: "Create, manage and track the performance of your ads.",
+  },
+  {
+    name: "Jobs",
+    icon: "jobs",
+    description: "Find a job that's right for you.",
+  },
+  {
+    name: "Climate science center",
+    icon: "climate",
+    description: "Learn about climate change and its effects.",
+  },
+  {
+    name: "COVID-19 Information Center",
+    icon: "covid",
+    description:
+      "See the latest prevention tips, community resources and updates from health organizations.",
+  },
+  {
+    name: "Community Help",
+    icon: "community",
+    description:
+      "Get involved in your community by creating a drive, requesting or offering help or volunteering.",
+  },
+  {
+    name: "Fundraisers",
+    icon: "fundraisers",
+    description: "Donate and raise money for nonprofits and personal causes.",
+  },
+  {
+    name: "Stoic Stocks",
+    icon: "messenger", // public/left/.png
+    description: "A live stock monitoring app with decent UI",
+  },
+  {
+    name: "Pranay Ki Picturey",
+    icon: "messenger", // public/left/.png
+    description: "Know more about your favourite movie and shows",
+  },
+  {
+    name: "Tinda",
+    icon: "messenger", // public/left/.png
+    description: "Swipe Left or Right, in a more interesting way",
+  },
+  {
+    name: "MyCycle",
+    icon: "messkids",
+    description: "You don't know about MyCycle ?? The friend of every woman",
+  },
+];
+export const create = [
+  {
+    name: "Post",
+    icon: "m_post_icon",
+  },
+  {
+    name: "Story",
+    icon: "m_story_icon",
+  },
+  // {
+  //   name: "Room",
+  //   icon: "m_room_icon",
+  // },
+  {
+    name: "Page",
+    icon: "m_page_icon",
+  },
+
+  {
+    name: "Ad",
+    icon: "m_ad_icon",
+  },
+  {
+    name: "Group",
+    icon: "m_group_icon",
+  },
+  {
+    name: "Event",
+    icon: "m_event_icon",
+  },
+  // {
+  //   name: "Marketplace Listing",
+  //   icon: "m_post_mar",
+  // },
+  {
+    name: "Recruitment",
+    icon: "m_post_job",
+  },
+];
